307363d9b7433dc09c6ec2062112ac1d89989d21
Umong Agarwalla <umong@sarvayxenia.com>
2021-12-29 12:35:33 +0530

    Install dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import * as React from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 function App() {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <h1>Robot Market</h1>
-      {/*Add your code here*/}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Robot Market</h1>
+        {/*Add your code here*/}
+      </div>
+    </QueryClientProvider>
   );
 }
 
