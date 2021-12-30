59c2aebdc2879145702f3bfb3b75904f3cadbde6
Umong Agarwalla <umong@sarvayxenia.com>
2021-12-30 12:13:03 +0530

    Fetch data from API

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,7 +1,11 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
+import {useQuery} from "react-query";
+import { fetchItemList } from '../../App.service';
 import './Home.scss';
 
 const Home = () => {
+    const {data} = useQuery('Items', () => fetchItemList());
+
     return (
         <div className="home">
             Home
